28ecf1225af044ea4a4467891c0c95ddd615c5c3
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2022-09-15 21:52:16 -0300

    initial commit

---
+++ Backend/src/router.ts
@@ -0,0 +1,10 @@
+
+import express from 'express';
+
+const server = express();
+
+server.get("/", (_, res) => {
+    res.send("Hello ts-node");
+})
+
+export default server
---
+++ Backend/src/server.ts
@@ -0,0 +1,5 @@
+import router from './router'
+
+router.listen(3333, () => {
+    console.log(`[SERVER] Running at http://localhost:3333`)
+})

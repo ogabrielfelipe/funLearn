3c2f187df593facc88bc8f599add30295df39ac7
lucas <lucasmartins0498@gmail.com>
2022-09-16 23:12:07 -0300

    Estruturação de pastas e criação de página de erro

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <>
-      <h1> Hello World </h1>
+      <h1> Página Inicial </h1>
     </>
   );
 }
---
+++ Frontend/src/screens/error/404.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Error404Screen: React.FC = () => {
+  return <>Not Found</>;
+};
+
+export default Error404Screen;

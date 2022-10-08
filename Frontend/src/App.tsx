a24eba48d05aea573e5d8db7325bf600ac2e30fc
lucas <lucasmartins0498@gmail.com>
2022-10-08 01:16:36 -0300

    estruturação do layout

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import Link from "next/link";
+
+import { Header, HeaderLogin, Background, ScreenStudent, ScreenTeacher } from "./styles"
+
 function App() {
   return (
-    <>
-      <h1> Página Inicial </h1>
-    </>
+  <>
+    <Header>
+      <HeaderLogin>
+        <h3>FunLearn</h3>
+        
+        <Link href="/admin">
+          <a>Administrador</a> 
+        </Link>
+      </HeaderLogin>
+        <Background>
+          <ScreenStudent>
+            <Link href="/student">
+              <a>Estudante</a>
+            </Link>
+          </ScreenStudent>
+
+          <ScreenTeacher>
+            <Link href="/teacher">
+              <a>Professor</a>
+            </Link>
+          </ScreenTeacher>
+        </Background>
+    </Header>
+  </>
   );
 }
 

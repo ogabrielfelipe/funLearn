7f230e1ecaa1ae0dbf3c64387abafddccf05c2b2
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2022-11-12 16:27:35 -0300

    Ajuste da busca de teacher no cadastro de turma no módulo professor.

--- frontend/src/pages/team/add/index.tsx
+++ frontend/src/pages/team/add/index.tsx
@@ -11,6 +11,7 @@ import { setupAPIClient } from "../../../services/api";
 import { canSSRAuth } from "../../../utils/canSSRAuth";
 import { Container } from "../styles";
 import { ContentButton, ContentForm, ContentInputForm, OptionSelect } from "./styles";
+import { destroyCookie, setCookie, parseCookies } from 'nookies'
 
 type TeamProps = {
     id: string,
@@ -41,7 +42,14 @@ export default function AddTeam( {teachers}: FindTeacherProps){
     const [teacherList, setTeacherList] = useState(teachers || [])
 
     const [nameTeam, setNameTeam] = useState("");
-    const [teacherSelected, setTeacherSelected] = useState("0");
+
+    var teacherSelectedPosition = "0";
+    const cookies = parseCookies()
+    if (cookies["@nextauth.type"] === "teacher"){
+        teacherSelectedPosition = cookies["@nextauth.user"]
+    }
+
+    const [teacherSelected, setTeacherSelected] = useState(teacherSelectedPosition);
     const [teamActive, setTeamActive] = useState("1");
 
     const [loading, setLoading] = useState(false);
@@ -195,7 +203,7 @@ export const getServerSideProps = canSSRAuth( async (ctx) => {
 
             return {
                 props:{
-                    teachers: res.data
+                    teachers: [res.data]
                 }
             }
         }else{

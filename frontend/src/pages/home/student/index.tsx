98a0495ed3fad80fcfc954b8bb9e7eb352a84a75
lucasmartinsribeiro <lucasmartins0498@gmail.com>
2023-01-03 17:29:17 -0300

    Começando a estruturar a página do aluno

--- frontend/src/pages/home/student/index.tsx
+++ frontend/src/pages/home/student/index.tsx
@@ -9,11 +9,21 @@ import { setupAPIClient } from "../../../services/api";
 import Link from 'next/link'
 
 import Head from "next/head";
+import Image from "next/image";
 import { HeaderAuth, HeaderStudent } from "../../../components/Header";
 
 import { Container } from "./styles";
 import { ButtonStudenTertiary, ButtonStudentPrimary, ButtonStudentSecondary } from "../../../components/Button";
 import { Header } from "../../../components/Header/styles";
+
+import { ButtonConfirmBlue } from "../../../components/Button";
+
+import TemaDoQuizz from "../../../../public/assets/Tema do Quizz.svg";
+import Frame from "../../../../public/assets/Frame.svg";
+import CaretLeft from "../../../../public/assets/CaretLeft.svg";
+import CaretRight from "../../../../public/assets/CaretRight.svg";
+import Description from "../../../../public/assets/Descrição.svg";
+import ArrowCounterClockwise from "../../../../public/assets/ArrowCounterClockwise.svg";
 
 export default function HomeStudent(){
     return (
@@ -23,8 +33,25 @@ export default function HomeStudent(){
             </Head>
             <HeaderStudent /> 
 
-            
+            <Container>
+                <Image src={TemaDoQuizz} layout="responsive"  alt={"Tema do Quizz."}/>
+                {/* <strong> TEMA DO QUIZZ</strong> */}
 
+                <Image src={Frame} layout="responsive"  alt={"Frame do Quizz."}/>
+
+                <Image src={CaretLeft} layout="responsive" />
+
+                <Image src={CaretRight} layout="responsive"  />
+
+                <Image src={Description} layout="responsive"  />
+                {/* <strong>Descrição do que é o quizz a ser respondido</strong> */}
+
+                <ButtonConfirmBlue type="submit">
+                    Começar
+                </ButtonConfirmBlue>
+
+                <Image src={ArrowCounterClockwise} layout="responsive"/>
+            </Container>
         </>
     )
 }

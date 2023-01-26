f769096869612ce8f67d0c3a38ea7742f254cf4a
Gabriel Felipe <ogabriel.v.felipe@gmail.com>
2023-01-25 22:55:15 -0300

    Ajuste

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function InitialView(){
 
                 <div className={styles.contentText}>
                     <strong className={styles.textPrincipal}> Bem-vindo ao ambiente de Aprendizagem de  <span style={{"color": "#8870FF"}}>Banco de Dados</span></strong>
-                    <span className={styles.descriptionText}> A Plataforma tem como principal objetivo, como o próprio nome já diz, é deixar o ensino de banco de dados de uma forma
+                    <span className={styles.descriptionText}> A Plataforma funLearn tem como principal objetivo, como o próprio nome já diz, é deixar o ensino de banco de dados de uma forma
                         mais descontraida e divertida para os alunos. </span>
                 
 
